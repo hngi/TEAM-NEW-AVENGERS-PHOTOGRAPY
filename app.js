69ff5f7e0225257a257fb1f5c5f0da1299be3ceb
prewsh <pngwube@gmail.com>
2020-06-30 18:10:50 +0100

    reset

--- app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.hamburger');
-const mobile = document.querySelector('.mobile');
-
-burger.addEventListener('click', () => {
-    mobile.classList.toggle('menuSlide')
-})
