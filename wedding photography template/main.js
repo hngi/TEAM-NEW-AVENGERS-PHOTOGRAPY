c8eb90d30a8a134424d1411b7209fb3a33aefbc1
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-07-01 19:47:00 +0100

    feat: implemented a wedding photography template page

---
+++ wedding photography template/main.js
@@ -0,0 +1,11 @@
+
+const hamburger = document.getElementById('hamburger');
+
+const menu = document.getElementById('menu')
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('show');   
+    menu.classList.toggle('show'); 
+    });
+
